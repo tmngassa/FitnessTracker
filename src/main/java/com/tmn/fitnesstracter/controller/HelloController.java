9003efa1440be8d443afa184cb0daeb5a8e98de6
tomson ngassa <tomson.ngassa@feisystems.com>
2015-09-04 11:18:00 -0400

    first

---
+++ src/main/java/com/tmn/fitnesstracter/controller/HelloController.java
@@ -0,0 +1,39 @@
+package com.tmn.fitnesstracter.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.tmn.fitnesstracter.HomeController;
+
+@Controller
+public class HelloController {
+	
+private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Say hello...", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("greeting", "Hello World!!" );
+		
+		return "hello";
+	}
+	
+}
